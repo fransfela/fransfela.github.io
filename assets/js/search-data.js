bf2be1e35fab58dcb4ae589ffa28c70bc8c0b45a
fransfela <randyrff@gmail.com>
2026-01-05 18:09:51 +0000

    Deploying to gh-pages from @ fransfela/fransfela.github.io@25895dd7ef52eade52bc0cbf815312c1130a13d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,12 +449,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+          section: "News",},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -494,6 +489,11 @@ ninja.data = [{
           description: "um projeto sem imagem",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
             },},{
         id: 'social-email',
         title: 'Send an email',
