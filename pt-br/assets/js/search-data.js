1a18450190829ea173f10141cbb1da31db8472af
fransfela <randyrff@gmail.com>
2026-01-05 18:10:35 +0000

    Deploying to gh-pages from @ fransfela/fransfela.github.io@28c55c6889274bff6160fd1b53cbd790ab71b4fa 🚀

--- pt-br/assets/js/search-data.js
+++ pt-br/assets/js/search-data.js
@@ -484,12 +484,7 @@ ninja.data = [{
             },},{id: "news-um-anúncio-simples-em-uma-linha-com-markdown-emoji-sparkles-smile",
           title: 'Um anúncio simples em uma linha com Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "Novidades",},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projetos",handler: () => {
-              window.location.href = "/pt-br/projects/8_project/";
-            },},{id: "projects-project-9",
+          section: "Novidades",},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projetos",handler: () => {
@@ -529,6 +524,11 @@ ninja.data = [{
           description: "with background image",
           section: "Projetos",handler: () => {
               window.location.href = "/pt-br/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projetos",handler: () => {
+              window.location.href = "/pt-br/projects/8_project/";
             },},{
         id: 'social-email',
         title: 'Enviar um email',
