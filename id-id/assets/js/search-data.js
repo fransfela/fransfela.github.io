474321cda1606162a80e1efd5eddbca3f4020127
fransfela <randyrff@gmail.com>
2026-01-13 22:25:26 +0000

    Deploying to gh-pages from @ fransfela/fransfela.github.io@46ea81174e9be6e1e231890e11d6eae0b1ffecac 🚀

--- id-id/assets/js/search-data.js
+++ id-id/assets/js/search-data.js
@@ -91,6 +91,17 @@ ninja.data = [{
             window.open("https://fransfela.substack.com/p/sound-horeg-antara-euforia-budaya", "_blank");
           
         },
+      },{id: "post-panduan-lengkap-beasiswa-luar-negeri-dari-kuli-pabrik-ke-denmark",
+        
+          title: "Panduan Lengkap Beasiswa Luar Negeri: Dari Kuli Pabrik ke Denmark",
+        
+        description: "Comprehensive guide untuk meraih beasiswa S2 dan S3 di luar negeri, based on real experience",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/id-id/blog/2025/panduan-beasiswa-luar-negeri/";
+          
+        },
       },{id: "post-soundscape-bentang-suara-teori-metode-dan-analisis",
         
           title: "Soundscape (Bentang Suara): Teori, Metode, dan Analisis",
